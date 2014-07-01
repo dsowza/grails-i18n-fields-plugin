6c9400ebf064bf6a4a2a35ec0123edf366848c25
Endika Gutiérrez <me@endika.net>
2014-07-01 14:00:08 +0200

    I18nFieldsGrailsPlugin.groovy reindented.

--- I18nFieldsGrailsPlugin.groovy
+++ I18nFieldsGrailsPlugin.groovy
@@ -9,57 +9,57 @@ import org.hibernate.event.PostInsertEvent
 class I18nFieldsGrailsPlugin {
     static final def log = Logger.getLogger(I18nFieldsGrailsPlugin)
 
-	def version = "0.9-redis-SNAPSHOT"
-	def grailsVersion = "2.1.0 > *"
-	def pluginExcludes = [
-			"lib/*",
-			"grails-app/i18n/*",
-			"grails-app/domain/i18nfields/*",
-			"grails-app/controllers/i18nfields/*",
-			"grails-app/services/i18nfields/*",
-			"grails-app/taglib/i18nfields/*",
-			"grails-app/views/*",
-			"grails-app/views/layouts/*",
-			"web-app/css/*",
-			"web-app/images/*",
-			"web-app/images/skin/*",
-			"web-app/js/*",
-			"web-app/js/prototype/*",
-	]
-	def dependsOn = [:]
+    def version = "0.9-redis-SNAPSHOT"
+    def grailsVersion = "2.1.0 > *"
+    def pluginExcludes = [
+            "lib/*",
+            "grails-app/i18n/*",
+            "grails-app/domain/i18nfields/*",
+            "grails-app/controllers/i18nfields/*",
+            "grails-app/services/i18nfields/*",
+            "grails-app/taglib/i18nfields/*",
+            "grails-app/views/*",
+            "grails-app/views/layouts/*",
+            "web-app/css/*",
+            "web-app/images/*",
+            "web-app/images/skin/*",
+            "web-app/js/*",
+            "web-app/js/prototype/*",
+    ]
+    def dependsOn = [:]
 
-	def config = ConfigurationHolder.config
+    def config = ConfigurationHolder.config
 
-	def author = "Jorge Uriarte"
-	def authorEmail = "jorge.uriarte@omelas.net"
-	def title = "i18n Fields"
-	def description = "This plugin provides an easy way of declarativily localize database fields of your content tables."
-	def documentation = "http://grails.org/plugin/i18n-fields"
+    def author = "Jorge Uriarte"
+    def authorEmail = "jorge.uriarte@omelas.net"
+    def title = "i18n Fields"
+    def description = "This plugin provides an easy way of declarativily localize database fields of your content tables."
+    def documentation = "http://grails.org/plugin/i18n-fields"
 
-	def grailsApplication
-	def setApplication(app) {
-		println "[i18nFields] Grails Application injected"
-		grailsApplication = app
-	}
+    def grailsApplication
+    def setApplication(app) {
+        println "[i18nFields] Grails Application injected"
+        grailsApplication = app
+    }
 
-	def doWithDynamicMethods = { context ->
-		println "[i18nFields] Plugin version: ${version}"
-		['domain', 'controller', 'service', 'bootstrap'].each {
-			println "[i18nFields] Adding 'withLocale' method to '${it}' classes"
-			application."${it}Classes".each { theClass ->
-				theClass.metaClass.withLocale = I18nFieldsHelper.withLocale
-			}
-		}
-		['domain'].each {  
-			println "[i18nFields] Adding saveLocale method to ${it} classes"
-			application."${it}Classes".each { theClass ->
-				theClass.metaClass.saveLocale = I18nFieldsHelper.saveLocale
-			}
-		}
+    def doWithDynamicMethods = { context ->
+        println "[i18nFields] Plugin version: ${version}"
+        ['domain', 'controller', 'service', 'bootstrap'].each {
+            println "[i18nFields] Adding 'withLocale' method to '${it}' classes"
+            application."${it}Classes".each { theClass ->
+                theClass.metaClass.withLocale = I18nFieldsHelper.withLocale
+            }
+        }
+        ['domain'].each {
+            println "[i18nFields] Adding saveLocale method to ${it} classes"
+            application."${it}Classes".each { theClass ->
+                theClass.metaClass.saveLocale = I18nFieldsHelper.saveLocale
+            }
+        }
         i18nfields.I18nFieldsHelper.metaClass.getApplicationContext = { -> context }
-	}
+    }
 
-	def doWithSpring = {
+    def doWithSpring = {
     }
 
     def doWithApplicationContext = { applicationContext ->
